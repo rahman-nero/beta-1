ac39575af963a38fb1396403fa5abed688e0fccc
Rahman_Nero <dikaevr053@gmail.com>
2020-04-23 17:53:19 +0300

    beta

--- js/lazyload.js
+++ js/lazyload.js
@@ -1,56 +1,56 @@
 // Lazy load
-alert('Try');
+alert(navigator.userAgent);
 
-try {
-  let lazyImages = [].slice.call(document.querySelectorAll("img.lazyload"));
-  if ("IntersectionObserver" in window && "IntersectionObserverEntry" in window && "intersectionRatio" in window.IntersectionObserverEntry.prototype) {
-    let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
-      entries.forEach(function(entry) {
-        if (entry.isIntersecting) {
-          let lazyImage = entry.target;
-          lazyImage.src = lazyImage.dataset.src;
-          // lazyImage.srcset = lazyImage.dataset.srcset;
-          lazyImage.classList.remove("lazyload");
-          lazyImageObserver.unobserve(lazyImage);
-        }
-      });
-    });
+// try {
+//   let lazyImages = [].slice.call(document.querySelectorAll("img.lazyload"));
+//   if ("IntersectionObserver" in window && "IntersectionObserverEntry" in window && "intersectionRatio" in window.IntersectionObserverEntry.prototype) {
+//     let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
+//       entries.forEach(function(entry) {
+//         if (entry.isIntersecting) {
+//           let lazyImage = entry.target;
+//           lazyImage.src = lazyImage.dataset.src;
+//           // lazyImage.srcset = lazyImage.dataset.srcset;
+//           lazyImage.classList.remove("lazyload");
+//           lazyImageObserver.unobserve(lazyImage);
+//         }
+//       });
+//     });
 
-    [].forEach.call(lazyImages,function(lazyImage) {
-      lazyImageObserver.observe(lazyImage);
-    }); 
+//     [].forEach.call(lazyImages,function(lazyImage) {
+//       lazyImageObserver.observe(lazyImage);
+//     }); 
 
-  }else{
-    var els = document.getElementsByClassName("lazyload");
+//   }else{
+//     var els = document.getElementsByClassName("lazyload");
 
-      Array.prototype.forEach.call(els, function(el) {
-         var img = el.src = el.dataset.src;
-         alert(img + '1');
-      });
-  }
+//       Array.prototype.forEach.call(els, function(el) {
+//          var img = el.src = el.dataset.src;
+//          alert(img + '1');
+//       });
+//   }
 
 
-} catch(e) {
+// } catch(e) {
   
- alert(e);
-}
+//  alert(e);
+// }
 
 
-  if (document.getElementsByClassName("lazyload")[0].src == false) {
+//   if (document.getElementsByClassName("lazyload")[0].src == false) {
 
-     var els = document.getElementsByClassName("lazyload");
+//      var els = document.getElementsByClassName("lazyload");
 
-      Array.prototype.forEach.call(els, function(el) {
-         var img = el.src = el.dataset.src;
-         alert(img);
-      });
+//       Array.prototype.forEach.call(els, function(el) {
+//          var img = el.src = el.dataset.src;
+//          alert(img);
+//       });
 
-      // [].forEach.call(document.getElementsByClassName('lazyload'), function(el) {
-      //   el.src = el.dataset.src;
-      // });
+//       // [].forEach.call(document.getElementsByClassName('lazyload'), function(el) {
+//       //   el.src = el.dataset.src;
+//       // });
 
 
-  }
+//   }
 
 
 
