b59d58412dceeb5a388afa4fec840f2a5b1646d9
Rahman_Nero <dikaevr053@gmail.com>
2020-04-23 15:29:31 +0300

    beta

--- js/lazyload.js
+++ js/lazyload.js
@@ -2,16 +2,6 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
   var lazyImages = [].slice.call(document.querySelectorAll("img.lazyload"));
-  var lazyImagesMobile = document.querySelectorAll("img.lazyload");
-
-  if (window.innerWidth < 1000) {
-    lazyImagesMobile.forEach(function(event){
-      var img = event.src = event.dataset.src;
-
-
-    });
-
-  }
   if ("IntersectionObserver" in window && "IntersectionObserverEntry" in window && "intersectionRatio" in window.IntersectionObserverEntry.prototype) {
     let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
       entries.forEach(function(entry) {
