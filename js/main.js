08f4d10fccd2b5aba2daf9466e9c7df31af00966
Rahman <dikaevr053@gmail.com>
2020-07-22 14:18:30 +0300

    upd css

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-$(window).scroll(function(){
-	if ($(window).scrollTop() >= 150) {
-		$('.block-header-top').hide(200);
-	} else if ($(window).scrollTop() <= 150){
-		$('.block-header-top').show(200);
-	}
-	console.log($(window).scrollTop());
-});
-
+//  Если ширина экрана больше 1200 пх, то показывваем верхний блок над меню
+if (screen.availWidth > 1200) {
+	$(window).scroll(function(){
+		if ($(window).scrollTop() >= 150) 
+			$('.block-header-top').hide(200);
+		else if ($(window).scrollTop() <= 150)
+			$('.block-header-top').show(200);
+	});
+}
 
 // Загрузка чата
 $('.loading_chat').fadeOut('slow');
--- js/slick_settings.js
+++ js/slick_settings.js
@@ -1,28 +1,26 @@
 if (screen.availWidth < 1000) {
-          $(".regular").slick({
-            dots: false,
-            slidesToShow: 1,
-            variableWidth: true,
-            slidesToScroll:1,   
-            // prevArrow: false,
-            // nextArrow : false,
-            centerMode: true,
-            prevArrow: '<div class="prev"><img src="img/slider/left.svg"></div>',
-            nextArrow : '<div class="next"><img src="img/slider/right.svg"></div>'
+      $(".regular").slick({
+        dots: false,
+        slidesToShow: 1,
+        variableWidth: true,
+        slidesToScroll:1,   
+        centerMode: true,
+        prevArrow: '<div class="prev"><img src="img/slider/left.svg"></div>',
+        nextArrow : '<div class="next"><img src="img/slider/right.svg"></div>'
 
-          });
+      });
 
-    }else{
-         $(".regular").slick({
-            dots: false,
-            slidesToShow: 6,
-            autoplay: true,
-            autoplaySpeed: 5000,
-            infinite: true,
-            variableWidth: true,
-            slidesToScroll:2,
-            prevArrow: '<div class="prev"><img src="img/slider/left.svg"></div>',
-            nextArrow : '<div class="next"><img src="img/slider/right.svg"></div>'
-        });
+}else{
+     $(".regular").slick({
+        dots: false,
+        slidesToShow: 6,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        infinite: true,
+        variableWidth: true,
+        slidesToScroll:2,
+        prevArrow: '<div class="prev"><img src="img/slider/left.svg"></div>',
+        nextArrow : '<div class="next"><img src="img/slider/right.svg"></div>'
+    });
 
-    }
+}
