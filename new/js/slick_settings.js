1871ee76aeddf2eeee9a69da5b44ed0cef790530
Rahman <dikaevr053@gmail.com>
2020-07-22 15:21:27 +0300

    upd design

---
+++ new/js/slick_settings.js
@@ -0,0 +1,27 @@
+if (screen.availWidth < 1000) {
+      $(".regular").slick({
+        dots: false,
+        slidesToShow: 1,
+        variableWidth: true,
+        slidesToScroll:1,   
+        centerMode: true,
+        lazyLoad : 'ondemand',
+        prevArrow: '<div class="prev"><img src="img/slider/left.svg"></div>',
+        nextArrow : '<div class="next"><img src="img/slider/right.svg"></div>',
+
+      });
+
+}else{
+     $(".regular").slick({
+        dots: false,
+        slidesToShow: 6,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        infinite: true,
+        variableWidth: true,
+        slidesToScroll:2,
+        prevArrow: '<div class="prev"><img src="img/slider/left.svg"></div>',
+        nextArrow : '<div class="next"><img src="img/slider/right.svg"></div>'
+    });
+
+}
