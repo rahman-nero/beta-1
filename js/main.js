8ecc45b6d671cf09a797ae273e68e3e1de840ffd
Rahman <dikaevr053@gmail.com>
2020-07-23 17:59:41 +0300

    Stylised anime block - add raiting and watch - blocks

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
-//  Если ширина экрана больше 1200 пх, то показывваем верхний блок над меню
 if (screen.availWidth > 1200) {
+   //  Если ширина экрана больше 1200 пх, то показывваем верхний блок над меню
 	$(window).scroll(function(){
 		if ($(window).scrollTop() >= 150) 
 			$('.block-header-top').hide(200);
 		else if ($(window).scrollTop() <= 150)
 			$('.block-header-top').show(200);
 	});
-}
+
+
+	// Если он незашел с мобилки, убираем звездочки со аниме - верхние звездочки
+	$('.raiting').remove();
+} 
 
 // Загрузка чата
 $('.loading_chat').fadeOut('slow');
