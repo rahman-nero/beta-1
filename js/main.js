0dd94da1aa960ab484e8db6f3b3a165570664d34
Rahman <dikaevr053@gmail.com>
2020-08-07 13:55:07 +0300

    фикс бага с меню

--- js/main.js
+++ js/main.js
@@ -1,44 +1,44 @@
-	// if (screen.availWidth > 1200) {
-	// 	//  Если ширина экрана больше 1200 пх, то показывваем верхний блок над меню
-	// 	 $(window).scroll(function(){
-	// 		 if ($(window).scrollTop() >= 150){
-	// 			 $('.block-header-top').hide(200);
-	// 		 }else if ($(window).scrollTop() <= 150) {
-	// 			 $('.block-header-top').show(200);
-	// 		 }
-	// 	 });
+	if (screen.availWidth > 1200) {
+		//  Если ширина экрана больше 1200 пх, то показывваем верхний блок над меню
+		 $(window).scroll(function(){
+			 if ($(window).scrollTop() >= 150){
+				 $('.block-header-top').hide(200);
+			 }else if ($(window).scrollTop() <= 150) {
+				 $('.block-header-top').show(200);
+			 }
+		 });
 	 
-	// 	 // Если он незашел с мобилки, убираем звездочки со аниме - верхние звездочки
-	// 	 $('.raiting').remove();
-	// 	 $('.mobile-modal').remove();
+		 // Если он незашел с мобилки, убираем звездочки со аниме - верхние звездочки
+		 $('.raiting').remove();
+		 $('.mobile-modal').remove();
 		 
-	// 	 //  скрипт с виджетом вк
-	// 	 $.getScript("library/slick_slider/slick.min.js", function() {
-	// 	 	$.getScript("js/slick_settings.js");
-	// 	 });
+		 //  скрипт с виджетом вк
+		 $.getScript("library/slick_slider/slick.min.js", function() {
+		 	$.getScript("js/slick_settings.js");
+		 });
 
-	// 	 $.getScript("https://vk.com/js/api/openapi.js?168", function() {
-	// 		 $.getScript("js/vk_widget.js"); 
-	// 	 });
+		 $.getScript("https://vk.com/js/api/openapi.js?168", function() {
+			 $.getScript("js/vk_widget.js"); 
+		 });
 
-	// 	 $('.timetable__block').on('click', '.button-open', (e) =>{
+		 $('.timetable__block').on('click', '.button-open', function(e){
 
-	// 	 	var block_time = $(e.target).next();
-	// 	 	var icon = $(e.target).find('.right_icon');
+		 	var block_time = $(e.target).next();
+		 	var icon = $(e.target).find('.right_icon');
 
-	// 	 	if ($(block_time).hasClass('open')) {
-	// 	 		$(block_time).removeClass('open');
-	// 	 		$(icon).removeClass('open');
-	// 	 	} else {
-	// 	 		$('.block-time').removeClass('open'); // до открытия, мы сначала закрываем все открытые секции
-	// 	 		$('.right_icon').removeClass('open');
+		 	if ($(block_time).hasClass('open')) {
+		 		$(block_time).removeClass('open');
+		 		$(icon).removeClass('open');
+		 	} else {
+		 		$('.block-time').removeClass('open'); // до открытия, мы сначала закрываем все открытые секции
+		 		$('.right_icon').removeClass('open');
 
-	// 	 		$(block_time).addClass('open');
-	// 	 		$(icon).addClass('open');
-	// 	 	}
-	// 	 });
+		 		$(block_time).addClass('open');
+		 		$(icon).addClass('open');
+		 	}
+		 });
 
-	//  }
+	 }
 	 
 	 // Загрузка чата
 	 $('.loading_chat').fadeOut('slow');
