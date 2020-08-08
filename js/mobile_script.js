b3212770b3dd3424d73fcc60a8485c30828be06d
Rahman <dikaevr053@gmail.com>
2020-08-08 10:55:37 +0300

    разделил скрипты для пк и для мобилок - js

---
+++ js/mobile_script.js
@@ -0,0 +1,16 @@
+
+$('.modal_menu').click(function(e){
+ e.preventDefault();
+ if ($('.mobile-modal').hasClass('open')) {
+	 $('.mobile-modal').removeClass('open');
+ } else {
+	 $('.mobile-modal').addClass('open');
+ }
+
+ if ($('.mobile-modal').hasClass('open')) {
+ 	$('.modal_menu').html("<img src='img/icons2/close.svg'>");
+ } else {
+ 	$('.modal_menu').html("<img src='img/header/menu.svg'>");
+ }
+});
+
